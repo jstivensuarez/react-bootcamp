b838108721f5894d0a6a6f5d84c2cb9c21ff7a01
stiven.suarez <stiven.suarez@yuxiglobal.com>
2021-11-10 17:13:27 -0500

    SnackBar added for simple messages.

--- src/components/PokemonButtons.js
+++ src/components/PokemonButtons.js
@@ -3,6 +3,7 @@ import { PokemonContext } from '../context/context';
 import catchImg from '../images/catch.PNG'
 import releaseImg from '../images/release.PNG'
 import PokemonContextActions from '../context/actions';
+import eventBus from '../eventBus';
 
 function PokemonButtons(props) {
 
@@ -12,10 +13,17 @@ function PokemonButtons(props) {
         if (!boxPokemon.find(p => p.id === selectedPokemon.id)) {
             const newPokemon = { ...selectedPokemon, position: boxPokemon.length }
             dispatch({ type: PokemonContextActions.setBoxPokemon, newPokemon });
+            eventBus.dispatch('showMessage', { message: `¡${newPokemon.name} is your new pokemon!` });
+        } else {
+            eventBus.dispatch('showMessage', { message: `You already have ${selectedPokemon.name}...`, type: 'warning' });
         }
     }
 
-    const onRelease = () => dispatch({ type: PokemonContextActions.removePokemonFromBox });
+    const onRelease = () => {
+        const pokemonName = selectedPokemon.name;
+        dispatch({ type: PokemonContextActions.removePokemonFromBox });
+        eventBus.dispatch('showMessage', { message: `¡${pokemonName} was released!` });
+    }
 
     return (
         <>
---
+++ src/components/PokemonSnackBar.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import { Snackbar } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import eventBus from '../eventBus';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const PokemonSnackBar = () => {
+    const [open, setOpen] = useState(false);
+    const [message, setMessage] = useState('');
+    const [severity, setSeverity] = useState('success');
+
+    useEffect(() => {
+        eventBus.on('showMessage', ({message, type}) => {
+            if(type){
+                setSeverity(type);
+            }else{
+                setSeverity('success');
+            }
+            setOpen(true);
+            setMessage(message);
+        });
+        return () => {
+            eventBus.remove('showMessage');
+        }
+    }, []);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    const action = (
+        <React.Fragment>
+            <IconButton
+                size='small'
+                aria-label='close'
+                color='inherit'
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize='small' />
+            </IconButton>
+        </React.Fragment>
+    );
+
+    return (
+        <Snackbar
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center', }}
+            open={open}
+            autoHideDuration={5000} onClose={handleClose}
+        >
+            <Alert severity={severity} sx={{ width: '100%' }} action={action}>
+                {message}
+            </Alert>
+        </Snackbar>
+    );
+}
+
+export default PokemonSnackBar;
